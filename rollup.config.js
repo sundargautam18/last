6e9fe25e4144ab644e65714c8cc564809fbbd699
sundargautam2055 <sundar.gautam@aitc.ai>
2025-03-06 14:45:39 +0545

    resolve::css

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss"; // Import the CSS plugin
 import packageJson from "./package.json" with { type: "json" };
 
 export default {
@@ -23,6 +24,7 @@ export default {
     resolve(),
     commonjs(),
     typescript({ tsconfig: "./tsconfig.json" }),
+    postcss({ extract: true }), // Enable CSS processing
   ],
   external: ["react", "react-dom"],
 };
